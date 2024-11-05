065c7ca2e5006ede6c5bd14d253be7040dcace81
Fábio A.  M.  Pereira <fabioamp@fastmail.com>
2024-11-05 17:37:34 -0300

    Adicionado parser para remover caracteres especiais,  menções,  links,  etc.,  dos posts.

--- utils/chan_html_parser.py
+++ utils/chan_html_parser.py
@@ -5,14 +5,33 @@ from html.parser import HTMLParser
 class ChanHTMLParser(HTMLParser):
     def __init__(self):
         super().__init__()
-        self.text = []
+        self.text = ""
 
     def handle_data(self, data):
-        # Replace \n and \r characters with a whitespace
-        data = re.sub(r"[\n\r]", " ", data)
-        # Only add text that does not match the ">>12345" pattern
-        if not re.match(r">>\d+", data.strip()):
-            self.text.append(data)
+        # Remove newlines, carriage returns, and quotation marks
+        data = data.replace("\n", "").replace("\r", " ").replace('"', " ").replace("==", " ").strip()
+        # Remove references to other posts (e.g., >>12345 or >12345)
+        data = re.sub(r">>\d+", "", data).strip()
+        data = re.sub(r">\d+", "", data).strip()
+        # Remove website links (e.g., https://fmhy.net/)
+        data = re.sub(r"http[s]?://\S+", "", data).strip()
+        # Remove standalone '>' symbols at the beginning of words
+        data = re.sub(r">\s*", " ", data).strip()
+        # Replace multiple whitespaces with a single whitespace
+        data = re.sub(r"\s+", " ", data).strip()
+        self.text = data
 
     def clear(self):
-        self.text = []  # Reset text to an empty list
+        self.text = ""  # Reset text to an empty list
+
+    def get_parsed_text(self):
+        return self.text
+
+
+# parser = ChanHTMLParser()
+# parser.feed(
+#     '>18014 E eu também demorei demais https://old.27chan.org/ pra agir, poderia ter poupado mais o https://www.youtube.com/watch?v=BFzL4q95kAY passarinho se eu não fosse bicha e não tivesse medinho de esmagar ele com algum objeto pesado. Por mim, a pá pode ter sido uma morte menos dolorosa, mas acho que só o tempo que eu demorei pra pegar a porra da pá e pensar em como matar pode ter feito ele sofrer mais. "apenas membros" fixe\'d. ==Limpeza realizada!==Dessa vez foram as boards que não foram abandonadas a serem limpas, acompanhe na thread de tábuas no >>>/mod/. >início de 2017 >faço a Fuvest em Ribeirão Preto >conheço um sujeito redpillado enquanto espero o portão abrir >falamos sobre o Trump >falamos sobre assuntos que eram discutidos nos chans da época >não falamos abertamente que somos anões >nem mencionamos o recinto uma única vez Eu tenho minhas dúvidas se o cara também era channer, mas é muita coincidência encontrar alguém que sabia com detalhes sobre a forçação do mês. Se estiver lendo isso, saiba que você é baseado.'
+# )
+# print("\nparsed_message:", parser.text)
+
+# parsed_message = parser.get_parsed_text()

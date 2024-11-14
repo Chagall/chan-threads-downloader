59ff4d61d1537d64e99bbeb03992e4e1a04ac1e6
Fábio A.  M.  Pereira <fabioamp@fastmail.com>
2024-11-13 23:49:41 -0300

    Adicionada lista de comandos necessários para usr ambiente virtual local (pyenv + virtualenv)

---
+++ commands.sh
@@ -0,0 +1,19 @@
+brew update
+brew install pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
+source ~/.zshrc  # Reload the shell configuration
+
+brew install pyenv-virtualenv
+pyenv virtualenv 3.12.7 chanvenv-3.12.7 
+pyenv local chanvenv-3.12.7 
+
+brew install direnv
+eval "$(direnv hook zsh)"  # or `bash` if you’re using Bash
+source ~/.zshrc
+
+echo 'layout python $(pyenv which python)' > .envrc
+direnv allow
+
+pip install -r requirements.txt
